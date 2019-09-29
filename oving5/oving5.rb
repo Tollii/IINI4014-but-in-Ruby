5cc4e0760aeda189792501fd4cf5bcf1813e6289
Tollii <andreas.tolnes@ymail.com>
2019-09-29 17:59:12 +0200

    A bit of refactoring

--- oving5/oving5.rb
+++ oving5/oving5.rb
@@ -4,11 +4,12 @@ module TextScanner
 	def self.get_word_frequency filename
 		Dir.chdir get_file_directory filename
 		word_hash = {}
-		file = File.open(filename)
-		file_data = file.readlines.map(&:chomp)
+		file = File.open filename
+		file_data = file.readlines.map &:chomp
 		file_data.each do |line| 
 			line.split.each do |word|
-				if word_hash.has_key? word.downcase; word_hash[word.downcase] += 1 else word_hash[word.downcase] = 1 end
+				downcase = word.downcase
+				if word_hash.has_key? downcase; word_hash[downcase] += 1 else word_hash[downcase] = 1 end
 			end
 		end
 		file.close
@@ -20,8 +21,8 @@ module TextScanner
 		Dir.chdir get_file_directory filename
 		line_counter = 0
 		line_list = []
-		file = File.open(filename)
-		file_data = file.readlines.map(&:chomp)
+		file = File.open filename
+		file_data = file.readlines.map &:chomp
 		file_data.each do |line|
 			line_counter += 1
 			if line.include? word; line_list.append line_counter end
@@ -35,14 +36,14 @@ module TextScanner
 	def self.get_file_directory filename
 		file_directory = nil
 		Dir.glob('**/*') { |f| file_directory = f.to_s if f.to_s.split("/")[-1] == filename}
-		file_directory = file_directory.split("/")
+		file_directory = file_directory.split "/"
 		file_directory.pop
-		return file_directory.join("/")
+		return file_directory.join "/"
 	end
 end
 
 #---------------------------
 
-p TextScanner::get_word_frequency "random.txt"
+#TextScanner::get_word_frequency "random.txt"
 #TextScanner::get_line_with_word "random.txt", "Ghost"
 
