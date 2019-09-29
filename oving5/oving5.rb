e424469a34946b5baa940c448984be06131b9921
Tollii <andreas.tolnes@ymail.com>
2019-09-29 05:55:24 +0200

    Started assignment 5

--- oving5/oving5.rb
+++ oving5/oving5.rb
@@ -1,36 +1,26 @@
 class TextScanner
-
 	def self.get_word_frequency filename
 		Dir.chdir get_file_directory filename
 		word_hash = {}
-
-
-
-
-
 	end
 
 	def self.get_line_with_word filename, word
 		Dir.chdir get_file_directory filename
-
-
-
-
 	end
 
 
-	#private
 
-	def self.get_file_directory filename
+	def get_file_directory filename
 		file_directory = nil
 		 Dir.glob('**/*') { |f| file_directory = f.to_s if f.to_s.split("/")[-1] == filename}
 		  file_directory = file_directory.split("/")
 		  file_directory.pop
 		  file_directory.join("/")
-
-
 	end
 end
 
 #---------------------------
+
+TextScanner.get_file_directory "84-0.txt"
+TextScanner.get_word_frequency "84-0.txt", "shall"
 
